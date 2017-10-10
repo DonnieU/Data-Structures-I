27e14e5afe4e9706efdb25d74a650e7af358c2f6
Biff <thedonnieutley@gmail.com>
2017-10-09 21:21:19 -0500

    insert, remove, & retrieve in hash table working. todo: hash table EC

--- src/hash-table.js
+++ src/hash-table.js
@@ -13,19 +13,44 @@ class HashTable {
   // If no bucket has been created for that index, instantiate a new bucket and add the key, value pair to that new bucket
   // If the key already exists in the bucket, the newer value should overwrite the older value associated with that key
   insert(key, value) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    if (this.storage[index] === undefined) {
+      this.storage[index] = [[key, value]];
+    } else if (this.storage[index]) {
+      let exists = false;
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          this.storage[index][i][0] = key;
+          this.storage[index][i][1] = value;
+          exists = true;
+        }
+      }
+      if (!exists) this.storage[index].push([key, value]);
+    }
   }
   // Removes the key, value pair from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Remove the key, value pair from the bucket
   remove(key) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    if (this.storage[index]) {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) {
+          this.storage[index][i].splice(i, 1);
+        }
+      }
+    } else return undefined;
   }
   // Fetches the value associated with the given key from the hash table
   // Fetch the bucket associated with the given key using the getIndexBelowMax function
   // Find the key, value pair inside the bucket and return the value
   retrieve(key) {
-
+    const index = getIndexBelowMax(key, this.limit);
+    if (this.storage[index]) {
+      for (let i = 0; i < this.storage[index].length; i++) {
+        if (this.storage[index][i][0] === key) return this.storage[index][i][1];
+      }
+    } else return undefined;
   }
 }
 
